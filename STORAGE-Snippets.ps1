fe35523ccb08ca26e1416825272972c1d0dce02b
Gary Mansell <Gary.Mansell@gmail.com>
2022-04-23 12:46:38 +0100

    Updated Storage

--- STORAGE-Snippets.ps1
+++ STORAGE-Snippets.ps1
@@ -1,4 +1,10 @@
-#Add a Storage Account firewall rule to allow access from a Subnet
+# Create a Storage account
+New-AzStorageAccount -Name "SA01" -ResourceGroupName "RG01" -SkuName Standard_GRS -Location "uksouth"
+
+# Get Storage Account Access Keys
+Get-AzStorageAccountKey -ResourceGroupName "RG01" -Name "SA01"
+
+# Add a Storage Account firewall rule to allow access from a Subnet
 # and then update it to also allow access for Azure Services such as Backup etc
 $subnet = Get-AzVirtualNetwork -ResourceGroupName "RG01" -Name "VNET01" | Get-AzVirtualNetworkSubnetConfig -Name "VSUBNET01"
 Add-AzStorageAccountNetworkRule -ResourceGroupName "RG01" -Name "storage01" -VirtualNetworkResourceId $subnet.Id
