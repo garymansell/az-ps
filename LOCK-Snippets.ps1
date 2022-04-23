9b116ae338c8b47ba42ed8a7fb7bf385cf0dfb50
Gary Mansell <Gary.Mansell@gmail.com>
2022-04-23 12:03:43 +0100

    LOCK-Snippets updated

--- LOCK-Snippets.ps1
+++ LOCK-Snippets.ps1
@@ -1,2 +1,8 @@
 #Create Resource Lock
 New-AzResourceLock –Locklevel CanNotDelete –LockName LockSite –ResourceName examplesite
+
+#Remove Resource Locks from all resources in an RG
+$RG = "rg1"
+Get-AzResourceLock | `
+Where-Object ResourceGroupName -EQ "$RG" |`
+Remove-AzResourceLock -Force
