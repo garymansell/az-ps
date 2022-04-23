e08fd62da344efc5051a150d7433bad59e85229c
Gary Mansell <Gary.Mansell@gmail.com>
2022-04-23 15:54:53 +0100

    dsc

---
+++ DSC.ps1
@@ -0,0 +1,22 @@
+# Create a configuration script for a VM and save as iisInstall.ps1
+configuration IISInstall
+{
+    node "localhost"
+    {
+        WindowsFeature IIS
+        {
+            Ensure = "Present"
+            Name = "Web-Server"
+        }
+    }
+}
+
+
+# Place the iisInstall.ps1 configuration script on the specified VM, execute and report back status
+$resourceGroup = 'dscVmDemo'
+$vmName = 'myVM'
+$storageName = 'demostorage'
+#Publish the configuration script to user storage
+Publish-AzVMDscConfiguration -ConfigurationPath .\iisInstall.ps1 -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force
+#Set the VM to run the DSC configuration
+Set-AzVMDscExtension -Version '2.76' -ResourceGroupName $resourceGroup -VMName $vmName -ArchiveStorageAccountName $storageName -ArchiveBlobName 'iisInstall.ps1.zip' -AutoUpdate -ConfigurationName 'IISInstall'
