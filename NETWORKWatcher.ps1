31fd3fa304ddecb34003befb3cecba35783daa09
Gary Mansell <Gary.Mansell@gmail.com>
2022-04-23 13:39:47 +0100

    updated network watcher

--- NETWORKWatcher.ps1
+++ NETWORKWatcher.ps1
@@ -9,9 +9,9 @@ Set-AzVMExtension `
   -TypeHandlerVersion "1.4"
 
 # Create a Network Watcher Packet Capture
-$vm=Get-AzVM -ResourceGroupName "RG01" -Name "VM01"
-$res=Get-AzResource | Where-Object {$_.ResourceType -eq "Microsoft.Network/networkWatchers" -and $_.Location -eq "uksouth"}
+$vm=Get-AzVM -ResourceGroupName "Gary-Admin-RG" -Name "gary-linux"
+$res=Get-AzResource | Where-Object {$_.ResourceType -eq "Microsoft.Network.network/Watchers" -and $_.Location -eq "uksouth"}
 $networkWatcher = Get-AzNetworkWatcher -Name $res.Name -ResourceGroupName $res.ResourceGroupName
-$diagnosticSA = Get-AzStorageAccount -ResourceGroupName "Diagnostics-RG" -Name "Diagnostics-Storage"
-$filter1 = New-AzPacketCaptureFilterConfig -Protocol TCP -RemoteIPAddress "0.0.0.0-255.255.255.255" -LocalIPAddress "10.0.0.3" -LocalPort "1-65535" -RemotePort "22"
+$diagnosticSA = Get-AzStorageAccount -ResourceGroupName "Gary-Admin-RG" -Name "garymansell"
+$filter1 = New-AzPacketCaptureFilterConfig -Protocol TCP -RemoteIPAddress "0.0.0.0-255.255.255.255" -LocalIPAddress "10.0.0.5" -LocalPort "1-65535" -RemotePort "22"
 New-AzNetworkWatcherPacketCapture -NetworkWatcher $networkWatcher -TargetVirtualMachineId $vm.id -PacketCaptureName "CaptureSFTP" -StorageAccountId $diagnosticSA.Id -TimeLimitInSeconds 60 -Filter $filter1
