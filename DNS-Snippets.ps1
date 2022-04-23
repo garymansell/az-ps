ee86b6c307c80e4453cf2a723a7abe6c975b80d5
Gary Mansell <Gary.Mansell@gmail.com>
2022-04-23 15:40:09 +0100

    bulk update

--- DNS-Snippets.ps1
+++
@@ -1,8 +0,0 @@
-#Azure Private DNS Zone & Link
-
-$zone = New-AzPrivateDnsZone -Name 'private.ps.com' `
--ResourceGroupName 'pluralsight-rg'
-
-$link = New-AzPrivateDnsVirtualNetworkLink -ZoneName 'private.ps.com' `
--ResourceGroupName 'pluralsight-rg' -Name 'hub-vnet' `
--VirtualNetworkId $vnet.id -EnableRegistration
