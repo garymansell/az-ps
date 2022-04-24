8f66851b53b74dde52ea8b304b5fad503291a03e
Gary Mansell <Gary.Mansell@gmail.com>
2022-04-24 13:30:55 +0100

    AZFILES

--- AZFILES.ps1
+++ AZFILES.ps1
@@ -1,2 +1,79 @@
 # Set Cloud Tiering for a server endpoint
 Set-AzStorageSyncServerEndpoint -ResourceId "serverendpointid" -CloudTiering $true -VolumeFreeSpacePercent 60
+
+
+
+$serverEndpointPath = "<your-server-endpoint-path>"
+$cloudTieringDesired = $true
+$volumeFreeSpacePercentage = <your-volume-free-space>
+# Optional property. Choose from: [NamespaceOnly] default when cloud tiering is enabled. [NamespaceThenModifiedFiles] default when cloud tiering is disabled. [AvoidTieredFiles] only available when cloud tiering is disabled.
+$initialDownloadPolicy = "NamespaceOnly"
+$initialUploadPolicy = "Merge"
+# Optional property. Choose from: [Merge] default for all new server endpoints. Content from the server and the cloud merge. This is the right choice if one location is empty or other server endpoints already exist in the sync group. [ServerAuthoritative] This is the right choice when you seeded the Azure file share (e.g. with Data Box) AND you are connecting the server location you seeded from. This enables you to catch up the Azure file share with the changes that happened on the local server since the seeding.
+
+
+# Create Sync Service
+$syncGroupName = "<my-sync-group>"
+$syncGroup = New-AzStorageSyncGroup -ParentObject $storageSync -Name $syncGroupName
+
+# Get or create a storage account with desired name
+$storageAccountName = "<my-storage-account>"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroup | Where-Object {
+    $_.StorageAccountName -eq $storageAccountName
+}
+
+if ($storageAccount -eq $null) {
+    $storageAccount = New-AzStorageAccount `
+        -Name $storageAccountName `
+        -ResourceGroupName $resourceGroup `
+        -Location $region `
+        -SkuName Standard_LRS `
+        -Kind StorageV2 `
+        -EnableHttpsTrafficOnly:$true
+}
+
+# Get or create an Azure file share within the desired storage account
+$fileShareName = "<my-file-share>"
+$fileShare = Get-AzStorageShare -Context $storageAccount.Context | Where-Object {
+    $_.Name -eq $fileShareName -and $_.IsSnapshot -eq $false
+}
+
+if ($fileShare -eq $null) {
+    $fileShare = New-AzStorageShare -Context $storageAccount.Context -Name $fileShareName
+}
+
+# Create the cloud endpoint
+New-AzStorageSyncCloudEndpoint `
+    -Name $fileShare.Name `
+    -ParentObject $syncGroup `
+    -StorageAccountResourceId $storageAccount.Id `
+    -AzureFileShareName $fileShare.Name
+
+
+if ($cloudTieringDesired) {
+    # Ensure endpoint path is not the system volume
+    $directoryRoot = [System.IO.Directory]::GetDirectoryRoot($serverEndpointPath)
+    $osVolume = "$($env:SystemDrive)\"
+    if ($directoryRoot -eq $osVolume) {
+        throw [System.Exception]::new("Cloud tiering cannot be enabled on the system volume")
+    }
+
+    # Create server endpoint
+    New-AzStorageSyncServerEndpoint `
+        -Name $registeredServer.FriendlyName `
+        -SyncGroup $syncGroup `
+        -ServerResourceId $registeredServer.ResourceId `
+        -ServerLocalPath $serverEndpointPath `
+        -CloudTiering `
+        -VolumeFreeSpacePercent $volumeFreeSpacePercentage `
+        -InitialDownloadPolicy $initialDownloadPolicy `
+        -InitialUploadPolicy $initialUploadPolicy
+} else {
+    # Create server endpoint
+    New-AzStorageSyncServerEndpoint `
+        -Name $registeredServer.FriendlyName `
+        -SyncGroup $syncGroup `
+        -ServerResourceId $registeredServer.ResourceId `
+        -ServerLocalPath $serverEndpointPath `
+        -InitialDownloadPolicy $initialDownloadPolicy
+}
