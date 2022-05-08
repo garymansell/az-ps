8c461e3938c7a04ab233f347a7b0f595afd3dadb
Gary Mansell <Gary.Mansell@gmail.com>
2022-05-08 13:31:33 +0100

    Updates

--- ARM.ps1
+++ ARM.ps1
@@ -9,3 +9,10 @@ New-AzResourceGroupDeployment -Name`
   -Name blanktemplate `
   -ResourceGroupName myResourceGroup `
   -TemplateFile $templateFile`
+
+
+# To deploy from a template (SAS.json) in a Storage account Container (private) with SAS token protection
+$templateuri = New-AzStorageBlobSASToken -Container "private" -Blob "SAS.json" -Permission r -ExpiryTime (Get-Date).AddHours(2.0) -FullURi
+New-AzResourGroup -Name "RG1" -Location "uksouth"
+New-AzResourceGroupDeployment -ResourceGroupName "RG1" -TemplateUri $templateuri
+
--- STORAGE.ps1
+++ STORAGE.ps1
@@ -9,3 +9,12 @@ Get-AzStorageAccountKey -ResourceGroupName "RG01" -Name "SA01"
 $subnet = Get-AzVirtualNetwork -ResourceGroupName "RG01" -Name "VNET01" | Get-AzVirtualNetworkSubnetConfig -Name "VSUBNET01"
 Add-AzStorageAccountNetworkRule -ResourceGroupName "RG01" -Name "storage01" -VirtualNetworkResourceId $subnet.Id
 Update-AzStorageAccountNetworkRuleSet -ResourceGroupName "RG01" -Name "storage01" -Bypass AzureServices
+
+
+# Only allow access to a Storage Account (storage1) Subnet (10.0.1.0/24) from the App Subnet (10.0.0.0/24)
+Update-AzStorageAccountNetworkRuleSet -ResourceGroupName "RG1" -Name "storage1" -DefaultAction Deny
+Get-AzVirtualNetwork -ResourceGroupName "RG1" -Name "VNet1" | \
+    Set-AzVirtualNetworkSubnetConfig -Name "appSubnet" -AddressPrefix "10.0.0.0/24" -ServiceEndpoint "Microsoft.Storage" | \
+    Set-AzVirtualNetwork
+$subnet = Get-AzVirtualNetwork -ResourceGroupName "RG1" -Name "VNet1" | Get-AzVirtualNetworkSubnetConfig -Name "appSubnet"
+Add-AzStorageAccountNetworkRule -ResourceGroupName "RG1" -Name "storage1" -VirtualNetworkResourceId $subnet.Id
